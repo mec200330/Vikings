2dcd7208a97036e795b4f0b5eb576bea81d7e60c
Cristhian <cristhiangomezdev@gmail.com>
2022-11-10 13:59:46 +0100

    Lab

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,43 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor (health, strength){
+
+        this.health = health
+        this.strength = strength
+    }
+    
+    
+    attack(){
+     return this.strength   
+    }
+    
+    receiveDamage(theDamage) {
+    this.health -= theDamage
+    }    
+    
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+
+    constructor (name, health, strength){
+        super(health, strength)
+        this.name=name
+        this.health=health
+        this.strength=strength
+    }
+    
+
+       receiveDamage(theDamage) {
+        this.health -= theDamage
+        if(this.health>0){
+            return (`${this.name} has received ${theDamage} points`);
+        }else{
+            return (`${this.name} has died in act of combat`);
+        }
+        }    
+        
+}
 
 // Saxon
 class Saxon {}
